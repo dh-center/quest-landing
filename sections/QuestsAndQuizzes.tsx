321f1586de95eeb0b636bf5110d3a05be9f03d80
ilyamore88 <themorozik@gmail.com>
2021-06-10 12:27:34 +0300

    fix quests

--- sections/QuestsAndQuizzes.tsx
+++ sections/QuestsAndQuizzes.tsx
@@ -2,14 +2,17 @@ import { ReactElement } from 'react';
 import styled from 'styled-components';
 import Section from '../components/lib/Section';
 import Title from '../components/lib/Title';
+import Container from '../components/lib/Container';
 
-const StyledSection = styled(Section)`
-  padding: 135px 0 0;
+const Phone = styled.img`
+  margin-top: 135px;
 `;
 
 const TextBlock = styled.div`
   display: inline-block;
   width: 460px;
+
+  margin-top: 135px;
   margin-left: auto;
 `;
 
@@ -27,22 +30,24 @@ const DescriptionParagraph = styled.p`
  */
 export default function QuestsAndQuizzes(): ReactElement {
   return (
-    <StyledSection>
-      <img src="/images/screenshots/quest-passing.png" alt="Скриншот процесса прохождения квеста"/>
-      <TextBlock>
-        <StyledTitle>
-          Квесты и квизы
-        </StyledTitle>
-        <DescriptionParagraph>
-          Проходя квесты и выполняя квизы, можно не только узнать много нового, но и проверить свою эрудицию.
-        </DescriptionParagraph>
-        <DescriptionParagraph>
-          В  наших квестах ты сможешь попробовать себя в роли сыщика, прожить один день вместе с любимым литературным персонажем или попытаться пройти по исторически опасному району и не попасть в неприятности.
-        </DescriptionParagraph>
-        <DescriptionParagraph>
-          Закрепляй новые знания и проверяй себя с помощью квизов, чтобы получить награды за правильные ответы.
-        </DescriptionParagraph>
-      </TextBlock>
-    </StyledSection>
+    <Section>
+      <Container>
+        <Phone src="/images/screenshots/quest-passing.png" alt="Скриншот процесса прохождения квеста"/>
+        <TextBlock>
+          <StyledTitle>
+            Квесты и квизы
+          </StyledTitle>
+          <DescriptionParagraph>
+            Проходя квесты и выполняя квизы, можно не только узнать много нового, но и проверить свою эрудицию.
+          </DescriptionParagraph>
+          <DescriptionParagraph>
+            В  наших квестах ты сможешь попробовать себя в роли сыщика, прожить один день вместе с любимым литературным персонажем или попытаться пройти по исторически опасному району и не попасть в неприятности.
+          </DescriptionParagraph>
+          <DescriptionParagraph>
+            Закрепляй новые знания и проверяй себя с помощью квизов, чтобы получить награды за правильные ответы.
+          </DescriptionParagraph>
+        </TextBlock>
+      </Container>
+    </Section>
   );
 }
