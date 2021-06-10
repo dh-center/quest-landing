41871e237ae28147e9078fc34bd4ce6e82424cf1
ilyamore88 <themorozik@gmail.com>
2021-06-10 12:31:24 +0300

    fix achievements

--- sections/AchievementsAndFriends.tsx
+++ sections/AchievementsAndFriends.tsx
@@ -3,14 +3,13 @@ import styled from 'styled-components';
 import Section from '../components/lib/Section';
 import Title from '../components/lib/Title';
 import Button from '../components/lib/Button';
-
-const StyledSection = styled(Section)`
-  padding: 135px 0 0;
-`;
+import Container from '../components/lib/Container';
 
 const TextBlock = styled.div`
   display: inline-block;
   width: 496px;
+
+  margin-top: 135px;
   margin-right: auto;
 `;
 
@@ -25,25 +24,31 @@ const DescriptionParagraph = styled.p`
   margin-bottom: 1em;
 `;
 
+const Phone = styled.img`
+  margin-top: 135px;
+`;
+
 /**
  * Achievements and friend section component
  */
 export default function AchievementsAndFriends(): ReactElement {
   return (
-    <StyledSection>
-      <TextBlock>
-        <StyledTitle>
-          Достижения и друзья
-        </StyledTitle>
-        <DescriptionParagraph>
-          Гуляй по Петербургу с нашим приложением и накапливай очки опыта. Выполняя задания, можно получать достижения и открывать премиальный контент.
-        </DescriptionParagraph>
-        <DescriptionParagraph>
-          Вместе веселее! Приглашай друзей в приложение, чтобы проходить квесты вместе, следить за их прогрессом и хвастаться своими ачивками.
-        </DescriptionParagraph>
-        <Button href='https://github.com/dh-center/quest'>Скачать</Button>
-      </TextBlock>
-      <img src="/images/screenshots/account.png" alt="Скриншот окна аккаунта"/>
-    </StyledSection>
+    <Section>
+      <Container>
+        <TextBlock>
+          <StyledTitle>
+            Достижения и друзья
+          </StyledTitle>
+          <DescriptionParagraph>
+            Гуляй по Петербургу с нашим приложением и накапливай очки опыта. Выполняя задания, можно получать достижения и открывать премиальный контент.
+          </DescriptionParagraph>
+          <DescriptionParagraph>
+            Вместе веселее! Приглашай друзей в приложение, чтобы проходить квесты вместе, следить за их прогрессом и хвастаться своими ачивками.
+          </DescriptionParagraph>
+          <Button href='https://github.com/dh-center/quest'>Скачать</Button>
+        </TextBlock>
+        <Phone src="/images/screenshots/account.png" alt="Скриншот окна аккаунта"/>
+      </Container>
+    </Section>
   );
 }
